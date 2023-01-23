cc7b1ba14e4eeef290887f3e0caef1e9d959cd0e
pandenata <pandedeleng@gmail.com>
2023-01-23 16:54:32 +0800

    Initial commit

---
+++ resources/views/code.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('content')
+<section>
+    <div class="conttainer mt-5">
+        <div class="row">
+            <div class="col-lg-8">
+<h3><center>CODINGAN PROJECT<center></h3>
+</div>
+          <div class ="col-lg-55">
+          <table class="table table-bordered border-primary">
+            <tr>
+            <center><img src="img/kode 1.png" width="1080px" ></center>    
+            <center><img src="img/kode 2.png" width="1080px" ></center>
+          
+
+            </tr>
+                    
+                    
+</section>
+@endsection
